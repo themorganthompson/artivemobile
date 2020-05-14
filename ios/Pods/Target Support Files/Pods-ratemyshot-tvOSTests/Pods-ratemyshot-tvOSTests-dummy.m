a4e8d19d828ed3e02ac1335cc22ff835fcc996a2
Morgan <morgan.thompson@advance-auto.com>
2020-05-14 07:35:03 -0400

    cleanup

--- ios/Pods/Target Support Files/Pods-ratemyshot-tvOSTests/Pods-ratemyshot-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ratemyshot_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_ratemyshot_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-ratemyshotTests/Pods-ratemyshotTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ratemyshotTests : NSObject
-@end
-@implementation PodsDummy_Pods_ratemyshotTests
-@end
